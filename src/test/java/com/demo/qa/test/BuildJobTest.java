3f52f0a2cc197f890c9b74510ad145dca9cb93d9
Sangwan70 <ramnsangwan@gmail.com>
2022-01-23 16:57:19 -0800

    Update BuildJobTest.java

--- src/test/java/com/demo/qa/test/BuildJobTest.java
+++ src/test/java/com/demo/qa/test/BuildJobTest.java
@@ -17,15 +17,14 @@ public class BuildJobTest {
 	public void setUp()
 	{
 System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
-            ChromeOptions options = new ChromeOptions();
-            options.addArguments("disable-infobars");
-            options.addArguments("--disable-browser-side-navigation");
-            options.addArguments("--start-maximized");
-            options.addArguments("--no-sandbox");
-            options.addArguments("--headless");
-            options.addArguments("--disable-dev-shm-usage");
-            options.setExperimentalOption("useAutomationExtension", false);
-		driver=new ChromeDriver();
+  		driver=new ChromeDriver();
+           driver.addArguments("disable-infobars");
+            driver.addArguments("--disable-browser-side-navigation");
+            driver.addArguments("--start-maximized");
+            driver.addArguments("--no-sandbox");
+            driver.addArguments("--headless");
+            driver.addArguments("--disable-dev-shm-usage");
+            driver.setExperimentalOption("useAutomationExtension", false);
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); //Implicit wait
 		driver.get("https://www.rnsangwan.com/");
