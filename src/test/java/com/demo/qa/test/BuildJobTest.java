9f9aaeb23c2fce329bcd0cb3987a24d1a0a9ab41
Sangwan70 <ramnsangwan@gmail.com>
2022-01-23 16:26:09 -0800

    Update BuildJobTest.java

--- src/test/java/com/demo/qa/test/BuildJobTest.java
+++ src/test/java/com/demo/qa/test/BuildJobTest.java
@@ -16,7 +16,7 @@ public class BuildJobTest {
 	@BeforeMethod
 	public void setUp()
 	{
-		System.setProperty("webdriver.chrome.driver", "/bin/google-chrome");
+		System.setProperty("webdriver.chrome.driver", "/bin/chromium-browser");
 		driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); //Implicit wait
